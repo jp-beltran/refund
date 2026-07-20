d4e3b6eb7ba028733518cef1bd50ed4dadad4abc
João Pedro Beltran <jpbcs2005@gmail.com>
2026-07-20 20:39:43 -0300

    feat: adicionando lógica de : - Contagem de despesas - Soma de preço de todas as despesas - Remover despesa - Limpar campos

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 const form = document.querySelector("form");
-const amount_input = document.getElementById("amount");
-const expense_input = document.getElementById("expense");
-const category_input = document.getElementById("category");
+const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+const totalAmount = document.querySelector("aside header p span");
+const totalValueAmount = document.querySelector("aside header h2");
 
 // Selecionar os elementos da lista
 const expenseList = document.querySelector("ul");
 
 // utils
 const formatToBRL = (value) => {
-  const cleanValue = String(value.replace(/[^0-9]/g, ""));
-  const centValue = Number(cleanValue) / 100;
-  return centValue.toLocaleString("pt-BR", {
+  if (typeof value === "string") {
+    const cleanValue = String(value.replace(/[^0-9]/g, ""));
+    const centValue = Number(cleanValue) / 100;
+    return centValue.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  }
+  return value.toLocaleString("pt-BR", {
     style: "currency",
     currency: "BRL",
   });
 };
 
 // Validando inputs
-amount_input.oninput = () => {
-  let value = formatToBRL(amount_input.value);
-  amount_input.value = value;
+amount.oninput = () => {
+  let value = formatToBRL(amount.value);
+  amount.value = value;
 };
 
 //Formulario
@@ -28,10 +36,10 @@ form.onsubmit = (event) => {
 
   const newExpense = {
     id: new Date().getTime(),
-    expense: expense_input.value,
-    category_id: category_input.value,
-    category_name: category_input.options[category_input.selectedIndex].text,
-    amount: amount_input.value,
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
     created_at: new Date(),
   };
   console.log("🚀 ~ newExpense:", newExpense);
@@ -77,12 +85,58 @@ const expenseAdd = (newExpense) => {
 
     expenseList.append(expenseItem);
     expenseInfo.append(expenseName, expenseCategory);
+
+    updateTotals();
+    onClearField();
   } catch (error) {
     alert("Não foi possível adicionar a lista de despesas");
     console.log(error);
   }
 };
 
-const onRemoveItem = (id) => {
+// Remover despesa
+expenseList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("remove-icon")) {
+    const item = event.target.closest(".expense");
+    item.remove();
+  }
+  updateTotals();
+});
 
-}
+// Atualizar o valor das despesas
+const updateTotals = () => {
+  try {
+    const items = expenseList.children;
+    totalAmount.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`;
+
+    let total = 0;
+    for (let item = 0; item < items.length; item++) {
+      const itemAmount = items[item].querySelector(".expense-amount");
+
+      let value = itemAmount.textContent
+        .replace(/[^\d,]/g, "")
+        .replace(",", ".");
+
+      //falback
+      if (isNaN(value)) {
+        return "O valor da despesa não é um número válido";
+      }
+
+      value = parseFloat(value);
+      total += value;
+    }
+    totalValueAmount.textContent = `${formatToBRL(total)}`;
+  } catch (error) {
+    console.log(error);
+    alert("Não foi possivel atualizar os totais");
+  }
+};
+
+// Limpar campos após criar uma despesa
+const onClearField = () => {
+  amount.value = "";
+  expense.value = "";
+  category.value = "";
+
+  expense.focus()
+};
